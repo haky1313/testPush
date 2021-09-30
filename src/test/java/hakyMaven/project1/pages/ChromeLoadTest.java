6b49525f7571265ac56b0c718727a0c074e926e0
haky1313 <haky1313@gmail.com>
2021-10-01 00:41:09 +0530

    test

---
+++ src/test/java/hakyMaven/project1/pages/ChromeLoadTest.java
@@ -0,0 +1,17 @@
+package hakyMaven.project1.pages;
+
+import org.testng.annotations.Test;
+
+public class ChromeLoadTest {
+	@Test
+	public void loadChromeDriver()
+	{
+		System.out.println("Loading Chrome Driver....");
+	}
+	
+	@Test
+	public void loginChromeTest()
+	{
+		System.out.println("Loging in from Chrome....");
+	}
+}
---
+++ src/test/java/hakyMaven/project1/pages/IELoadTest.java
@@ -0,0 +1,17 @@
+package hakyMaven.project1.pages;
+
+import org.testng.annotations.Test;
+
+public class IELoadTest {
+	@Test
+	public void loadIEDriver()
+	{
+		System.out.println("Loading IE Driver....");
+	}
+	
+	@Test
+	public void loginIETest()
+	{
+		System.out.println("Loging in from IE....");
+	}
+}
